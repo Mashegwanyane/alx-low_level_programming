c6c32306887c4e49ccbdebe4638bd10b1255d44b
Mashegwanyane <Lmashegwanyane@gmail.com>
2023-03-16 19:40:55 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+/**
+ * main - A program that print a line using the print f function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
