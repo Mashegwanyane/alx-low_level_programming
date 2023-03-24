1e78e3b6ff14d0eba021eeb99dc7ea2be46ee10f
Mashegwanyane <Lmashegwanyane@gmail.com>
2023-03-23 19:03:14 -0700

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * Print_numbers -Print the numbers since 0 up to 9
- * Return: The numbers since 0 up to 9
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Description: function that prints the numbers, from 0 to 9,
+ * followed by a new line.
+ *
+ * Return: void
  */
 
 void print_numbers(void)
 {
+	int number;
 
-	int x;
-
-	for (x = 0; x <= 0; x++)
-	{
-		_putchar(x + '0');
-	}
+	for (number = '0'; number <= '9'; number++)
+		_putchar(number);
 	_putchar('\n');
 }
